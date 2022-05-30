3fc2011b8f13926fd02d678bf7ff8aab6c5a37e4
nbysingh <nbysingh@gmail.com>
2022-05-31 00:31:31 +0530

    Added Validation - closes #8

--- src/main/java/com/nbysigh/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/nbysigh/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,14 +17,25 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+    @Null
     private UUID id;
+    @Null
     private Integer version;
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
+    @NotNull
     private String beerName;
+    @NotNull
     private BeerStyleEnum beerStyle;
+    @Positive
+    @NotNull
     private Long upc;
+    @Positive
+    @NotNull
     private BigDecimal price;
+
     private Integer quantityOnHand;
 }
 
